cf40b186069b620cee5dd3c76d6c3f60e540fee8
Matthew <matthewabebe.inbox@gmail.com>
2019-01-17 15:50:43 -0600

    first commit

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react' 
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <section>5555 Place St. Nashville North, 55555</section>
+                <section>2222 Here St. Nashville South, 22222</section>
+            </article>
+        );
+    }
+}
